108334862e852348a84c70dd2d2c0f4625f31f4c
yamuna <yamuna@pyxeda.ai>
2021-08-23 14:15:06 +0530

    initial folder structure added for chapter 3

---
+++ Chapter3/Text/text_read.py
@@ -0,0 +1,2 @@
+# TODO: How to read text data (like log files) in python and print word by word or sentence by sentence
+# TODO: Code should be well commented.
---
+++ Chapter3/Visualization/data_visualization.py
@@ -0,0 +1,2 @@
+# TODO: How to visualize a histogram and a bar plot of a column (child vs adult)
+# TODO: Code should be well commented.
