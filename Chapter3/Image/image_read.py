08367b3bc97dfc3b44ae8f74db619783e67b93e5
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-08-23 17:30:36 +0530

    read an image in python and display it

--- Chapter3/Image/image_read.py
+++ Chapter3/Image/image_read.py
@@ -1,2 +1,24 @@
-# TODO: How to read an image in python and display it
-# TODO: Code should be well commented.
+# Python program to read an image and display it
+
+# Import OpenCV(cv2) module 
+import cv2
+
+# Please change the file location as needed
+file_location = 'E:\MiddleSchoolCurriculum\Chapter3\Image\dog_1.jpeg'
+
+try:
+  # Read the image
+  image = cv2.imread(file_location) 
+
+  # Output image with window name as 'Image' 
+  cv2.imshow('Image', image) 
+
+  # Maintain output window utill user presses a key 
+  cv2.waitKey(0)		 
+
+  # Destroying present windows on screen 
+  cv2.destroyAllWindows() 
+
+except:
+  print ('Cannot find the file location. Please provide the right file location')
+
