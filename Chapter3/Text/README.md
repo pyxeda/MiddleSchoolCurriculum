60297f6c09aa4ca5a33ca2ff359799a479c1a2fb
naveen.atukorala@pyxeda.ai <naveen.atukorala@pyxeda.ai>
2021-08-26 15:33:17 +0530

    read text file and print the sentences or words

--- Chapter3/Text/README.md
+++ Chapter3/Text/README.md
@@ -1,1 +1,23 @@
-This readme should describe 'what is in the python code'.
+
+What it does :
+
+Read text data in python and print word by word or sentence by sentence.
+
+Dependancies :
+
+These dependancies are needed only for printing a text document, sentence by sentence
+
+1.nltk module is needed to be installed in the local machine to run this program.
+
+2.Download 'punkt' module using nltk
+  open a python command line and run the following code
+  import nltk
+  nltk.download('punkt')
+
+Why do we need 'punkt' module : 
+
+The sent_tokenize function uses an instance of PunktSentenceTokenizer from the nltk.tokenize.punkt module, which is already well trained. So it knows to mark the end and beginning of a sentence properly.
+
+Things to check before running :
+
+Check whether you have given the correct location of your text file.
--- Chapter3/Text/text_read.py
+++ Chapter3/Text/text_read.py
@@ -1,2 +1,42 @@
-# TODO: How to read text data (like log files) in python and print word by word or sentence by sentence
-# TODO: Code should be well commented.
+# Read text data (like log files) in python and print word by word or sentence by sentence
+
+# The execution of this code will print a text document, sentence by sentence.
+
+# Import nltk module
+import nltk
+
+try:
+    # Open the file in the specified location
+    with(open(r'about_us.txt') as file):
+        # Read the file
+        text = file.read()
+        # Use sent_tokenize function in nltk to identify each sentance separately in your text file
+        senetences = nltk.sent_tokenize(text)
+        # Print each sentence in the list
+        for senetence in senetences:
+            print(senetence)
+# Ask the user to input the correct location, if the location is incorrect
+except Exception as e:
+    print('Can not find the file location. Please provide the correct file location!')
+    print(e)
+
+
+# The execution of the below code will print a text document, word by word. You can uncomment the below code and comment out the above code if you want to run this.
+
+# try:
+#     # open file by giving the correct file location.
+#     with open(r'about_us.txt') as file:
+#         # We keep a word count, just to check all the words are printed
+#         word_count = 0
+#         # Iterate through each line of the file
+#         for line in file:
+#             # Iterate through each word of a line after splitting the line into a list
+#             for word in line.split():
+#                 # Add word count for every printed word.
+#                 word_count = word_count + 1
+#                 print('word : ', word_count)
+#                 print(word)
+# # Ask the user to provide the correct file location, if the location is incorrect
+# except:
+#     print('Can not find the file location. Please provide the correct file location!')
+
