cd22e7cd7413ab420d42178b8ef20ea0372926bc
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-08-26 14:38:16 +0530

    add spaces

--- Chapter3/Tabular/tabulardata_read.py
+++ Chapter3/Tabular/tabulardata_read.py
@@ -1,15 +1,21 @@
 # python program to read and display tabular data
+
 # import modules
 import pandas as pd
 import os
+
 # get the current working directory
 dir = os.path.dirname(__file__)
+
 # Please change the filename as needed
 filename = 'iris_dataset.csv'
+
 file_location = os.path.join(dir, filename)
+
 try:
     # read the CSV file
     data = pd.read_csv(file_location)
+
     # print a sample (first row) from the tabular data
     print(data.head(1))
     
@@ -17,6 +23,7 @@ try:
     column_names = data.columns
     for column in column_names:
         print (column)
+
     # provide the specific column name that you want to display
     column_name = 'petal_length'
     
@@ -24,8 +31,10 @@ try:
     if column_name in column_names:
         column = data[[column_name]]
         print (column)
+        
     else:
         print ('Column name cannot be found. Please provide a correct column name')
+
 except:
     print ('File cannot be found. Please provide the correct location')
 
