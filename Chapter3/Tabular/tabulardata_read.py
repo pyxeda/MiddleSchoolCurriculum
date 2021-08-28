2d8af1220f8fdacf245118e2b7f424f86c36e4e2
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-08-28 22:21:46 +0530

    update readme

--- Chapter3/Tabular/tabulardata_read.py
+++ Chapter3/Tabular/tabulardata_read.py
@@ -8,7 +8,7 @@ import os
 dir = os.path.dirname(__file__)
 
 # Please change the filename as needed
-filename = 'iris_dataset.csv'
+filename = 'child vs adult.csv'
 
 file_location = os.path.join(dir, filename)
 
@@ -17,24 +17,29 @@ try:
     data = pd.read_csv(file_location)
 
     # print a sample (first row) from the tabular data
+    print('Sample (first row) from the dataset : ')
     print(data.head(1))
     
     # see the column names in the table
     column_names = data.columns
+    print('Column names in the table : ')
     for column in column_names:
         print (column)
 
-    # provide the specific column name that you want to display
-    column_name = 'petal_length'
+    # you can change the column name as you needed, after visulaising the column names
+    column_name = 'Child'
     
     # print specific column data from the tabular data
+    print('Data from the column ' + column_name + ' : ')
     if column_name in column_names:
         column = data[[column_name]]
         print (column)
         
+    # if you have provided a column name which cannot be seen under 'Column names in the table : ',
     else:
         print ('Column name cannot be found. Please provide a correct column name')
 
-except:
-    print ('File cannot be found. Please provide the correct location')
+except Exception as e:
+    print ('File cannot be found. Please provide the correct file name!')
+    print (e)
 
