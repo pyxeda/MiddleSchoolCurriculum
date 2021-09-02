0387cd05e79ae1520d70eb80968cd3e8cb3a6a01
naveen.atukorala@pyxeda.ai <naveen.atukorala@pyxeda.ai>
2021-09-02 18:46:07 +0530

    source file location changed to google drive, standardized the commenting

--- Chapter3/Audio/audio_read.py
+++ Chapter3/Audio/audio_read.py
@@ -1,27 +1,52 @@
 # Python program to read an audio file in a specific location and play it
 
-# Import mixer from pygame module
+# Import mixer to play audio
 from pygame import mixer
 
+# Import gdown module to download files from google drive
+import gdown
+
+#--------------------------------------------- Get the file location from google drive ----------------------------------------
+    
+# Please change the URL as needed (make sure you have the access to the file)
+
+url = 'https://drive.google.com/file/d/14A91Km8obCo7ohpfEr4_Nel5d8uKPiAl/view?usp=sharing'
+
+# Derive the file id from the URL
+file_id = url.split('/')[-2]
+
+# Derive the download url of the the file
+download_url = 'https://drive.google.com/uc?id=' + file_id
+
+# Give the location you want to save it in your local machine
+file_location = r'children_1.mp3'
+
+#--------------------------------------------- Download the audio file and play it --------------------------------------------
+
 try:
+    # Download the file from drive to your local machine
+    gdown.download(download_url, file_location)
+
     # Initialize the mixer
     mixer.init()
 
     # Load the music file by giving the location
-    mixer.music.load(r'children_1.mp3')
+    mixer.music.load(file_location)
 
     # Start playing the audio
     mixer.music.play()
 
-    # infinite loop
+    # Infinite loop
     while True:
         print("Press 'p' and enter to pause, 'r' and enter to resume")
         print("Press 'e' and enter to exit the program")
+
+        # Check keyboard input
         keyboard_input = input(" ")
         
         if keyboard_input == 'p':
             # Pausing the audio
-            mixer.music.pause()	
+            mixer.music.pause()
         elif keyboard_input == 'r':
             # Resuming the audio
             mixer.music.unpause()
@@ -29,9 +54,9 @@ try:
             # Stop the audio
             mixer.music.stop()
             break
-# Catch the keyboard Intercept action separately
+# Catch the keyboard Intercept action
 except KeyboardInterrupt:
-    print('Program killed by user.')
-# Ask the user to input correct location, if file location is incorrect
-except:
-    print('Can not find the audio file. Please provide the correct location!')
+    print('Program stopped by user.')
+# Notifying the user about the error
+except Exception as e:
+    print('File location or format incorrect/ You do not have the access')
