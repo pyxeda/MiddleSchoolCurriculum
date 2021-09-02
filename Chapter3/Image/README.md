89dad361788670141b11971e4bc6a42eeffb498f
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-09-02 17:30:41 +0530

    read image files from google drive

--- Chapter3/Image/README.md
+++ Chapter3/Image/README.md
@@ -1,15 +1,16 @@
 What it does :
 
-    1. Reads an image in the provided file location, and then displays it.
+    1. Reads an image from the google drive, and then displays it.
 
 Dependancies :
 
-Dependancies are needed for the image processing, and to get the path of the working directory
-
     1. OpenCV(cv2) module is needed to be installed in the local machine, for the image processing.
 
-    2. os module is a part of the standard library. No need to install it.
+    2. gdown module is needed to be installed in the local machine, to download the image file from the google drive. 
+
 
 Things to check before running :
 
-    1. Check whether you have given the correct file name of the image.
+    1. Check whether you have given the correct file location of the image. 
+
+    2. Make sure you have the access to the file. 
--- Chapter3/Image/image_read.py
+++ Chapter3/Image/image_read.py
@@ -1,17 +1,34 @@
 # Python program to read an image and display it
 
+
+# Import opencv module to read and display images
 import cv2
-import os
 
-# get the current working directory
-dir = os.path.dirname(__file__)
+# Import gdown module to download files from google drive
+import gdown
 
-# Please change the filename as needed
-filename = 'dog_1.jpeg'
 
-file_location = os.path.join(dir, filename)
+# -------------------------- Get the file location from the google drive. ---------------------------------
+
+# Please change the url as needed (make sure you have the access to the file)
+url = 'https://drive.google.com/file/d/1CokCuNkyP1zvuTXj_-hxIwevXz4TG9SA/view?usp=sharing'
+
+# Derive the file id from the url
+file_id = url.split('/')[-2]
+
+# Derive the download url of the file
+download_url = 'https://drive.google.com/uc?id=' + file_id
+
+# Give the location you want to save it in your local machine
+file_location = r'dog.png'
+
+
+# --- Downloading, Reading and Displaying of the image from the provided file location, will start now. ---
 
 try:
+  # Download the file from drive to your local machine
+  gdown.download(download_url, file_location)
+  
   # Read the image
   image = cv2.imread(file_location) 
 
@@ -25,5 +42,5 @@ try:
   cv2.destroyAllWindows() 
 
 except Exception as e:
-  print ('File cannot be found. Please provide the correct file name!')
-  print (e)
+  # Notifying the user about the error
+  print ("File location or format incorrect/ You don't have the access!")
