308ae2d45330d5be2532cd44c1336ec9aa53dab4
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-09-02 18:20:14 +0530

    use gdown module to get the file from google drive

--- Chapter3/Visualization/data_visualization.py
+++ Chapter3/Visualization/data_visualization.py
@@ -1,91 +1,108 @@
 # python program to visualize histograms and bar plots of data columns
     
-# import modules
+# import pandas module to read the CSV file and to process the tabular data
 import pandas as pd
+
+# Import matplotlib module for the data visualization
 import matplotlib.pyplot as plt
-import os
 
-# get the current working directory
-dir = os.path.dirname(__file__)
+# Import gdown module to download files from google drive
+import gdown
 
-# Please change the filename as needed
-filename = 'child vs adult.csv'
 
-file_location = os.path.join(dir, filename)
+# -------------------------- Get the file location from the google drive. ---------------------------------
+
+# Please change the url as needed (make sure you have the access to the file)
+url = 'https://drive.google.com/file/d/1aKqueRc7yqB0ugTU6VrjG0oGnddq37bM/view?usp=sharing'
+
+# Derive the file id from the url
+file_id = url.split('/')[-2]
+
+# Derive the download url of the file
+download_url = 'https://drive.google.com/uc?id=' + file_id
+
+# Give the location you want to save it in your local machine
+file_location = r'child vs adult.csv'
+
+
+# ---- Downloading, Reading and Visualization of data from the provided file location, will start now. ----
 
 try:
-    # read the CSV file
+    # Download the file from drive to your local machine
+    gdown.download(download_url, file_location)
+
+    # Read the CSV file
     data = pd.read_csv(file_location)
 
-    # print the details of the dataset including the column names. 
-    # you can use these details when selecting the column names for the following plots.
-    print ('Details of the dataset:')
+    # Print the details of the dataset including the column names. 
+    # You can use these details when selecting the column names for the following plots.
+    print ('-------------- Details of the Dataset --------------')
     data.info()
 
-    # get the column names of the dataset
+    # Get the column names of the dataset
     column_names = data.columns
     
-    # ------------------------------------------------------------------------------------------------------------ #
+    
     # ------------------------------------------- Plot the Histogram --------------------------------------------- #
-    # ------------------------------------------------------------------------------------------------------------ #
-
-    # select a column name to plot the hoistogram, after visulaising the details of the dataset.
+   
+    # Select a column name to plot the hoistogram, after visulaising the details of the dataset.
     histogram_data = 'Child'
 
-    # visualize the histogram of the selected column
+    # Visualize the histogram of the selected column
     if  histogram_data in column_names:
         
-        # get the data of the selected column
+        # Get the data of the selected column
         x = data[histogram_data]
 
-        # plot the histogram (you can change the number of bins and the color as you needed)
+        # Plot the histogram (you can change the number of bins and the color as you needed)
         plt.hist(x, bins = 30, color = 'green')
 
-        # set the title, x-axis name and the y-axis name of the plot
+        # Set the title, x-axis name and the y-axis name of the plot
         plt.title('Histogram for ' + histogram_data + 's')
         plt.xlabel('Number of ' + histogram_data + 's')
         plt.ylabel('Frequency')
         
-        # show the plot
+        # Show the plot
         plt.show()
 
+    # If you have provided a column name which cannot be seen under 'Details of the Dataset',
     else:
         print ('Column name to plot the histogram cannot be found. Please provide a correct column name!')
 
-    # ------------------------------------------------------------------------------------------------------------ #
+    
     # ------------------------------------------- Plot the Bar Graph --------------------------------------------- #
-    # ------------------------------------------------------------------------------------------------------------ #
 
-    # select the column names to plot the bar graph, after visulaising the details of the dataset
-    # according to the dataset you can change the number of y axis columns as you needed
+    # Select the column names to plot the bar graph, after visulaizing the details of the dataset
+    # According to the dataset you can change the number of y axis columns as you needed
     column_x_axis = 'Year of registration'
     column_y_axis_1 = 'Adult'
     column_y_axis_2 = 'Child'
     
-     # visualize the bar plot of selected columns
+    # Visualize the bar plot of selected columns
     if column_x_axis and column_y_axis_1 and column_y_axis_2 in column_names:
         
-        # create the dataframe
+        # Create the dataframe
         df = pd.DataFrame({
         column_x_axis: data[column_x_axis],
         column_y_axis_1: data[column_y_axis_1],
         column_y_axis_2: data[column_y_axis_2]})
 
-        # plot the bar graph
+        # Plot the bar graph
         df.plot(x=column_x_axis, y=[column_y_axis_1, column_y_axis_2], kind="bar")
 
-        # set the title, x-axis name and the y-axis name of the plot
-        plt.title('Bar graph for ' + filename)
+        # Set the title, x-axis name and the y-axis name of the plot
+        plt.title('Bar graph for ' + file_location)
         plt.xlabel(column_x_axis)
         plt.ylabel('Frequency')
 
-        # show the plot
+        # Show the plot
         plt.show()
 
+    # If you have provided column names which cannot be seen under 'Details of the Dataset',
     else:
         print ('Column names to plot the bar graph cannot be found. Please provide column names correctly!')
 
 except Exception as e:
-    print ('File cannot be found. Please provide the correct file name!')
-    print (e)
+  # Notifying the user about the error
+  print ("File location or format incorrect/ You don't have the access!")
 
