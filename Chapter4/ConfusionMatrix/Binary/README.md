d9ecc3421cdfef21cee77a695a2dc3becb0fb337
naveen.atukorala@pyxeda.ai <naveen.atukorala@pyxeda.ai>
2021-09-03 15:17:33 +0530

    #14 calculate confusion matrix - binary case

--- Chapter4/ConfusionMatrix/Binary/README.md
+++ Chapter4/ConfusionMatrix/Binary/README.md
@@ -1,1 +1,12 @@
-This readme should describe 'what is in the python code'.
+What it does :
+
+    1. This Python program take two lists, list _1 is labels and list_2 is AI predictions, calculate the confusion matrix (binary case).
+
+Dependancies :
+
+    2. pandas module is needed to be installed in the local machine to run this program.
+
+Things to check before running :
+
+    1. Check whether the given two lists have same numbers of items.
+    2. Check whether you include items with same item type inside the list.
--- Chapter4/ConfusionMatrix/Binary/confusionmatrix_calculate.py
+++ Chapter4/ConfusionMatrix/Binary/confusionmatrix_calculate.py
@@ -1,2 +1,35 @@
-# TODO: Give two lists. list _1 is labels and list_2 is AI predictions, calculate the confusion matrix (binary case)
-# TODO: Code should be well commented.
+# Python program to calucalte the confusion matrix for binary classification
+
+# Import pandas to calculate confusion matrix
+import pandas as pd
+
+#--------------------------------------------- Lists --------------------------------------------------------------------
+
+# Actuals
+actuals = [1, 1, 1, 1, 1, 0, 0, 1, 1, 1]
+
+# Predictiions
+predictions = [1, 0, 1, 0, 1, 0, 1, 1, 1, 1]
+
+#--------------------------------------------- Calculate the Confusion Matrix --------------------------------------------
+
+try:
+    # Check whether both lists contain the same number of elements
+    if len(actuals) == len (predictions):
+        
+        # Convert actual list to a series
+        actuals = pd.Series(actuals, name='Actual')
+        
+        # Convert prediction list to a series
+        predictions = pd.Series(predictions, name='Predictions')
+        
+        # Create Confusion Matrix
+        confusion_matrix = pd.crosstab(predictions, actuals)
+
+        # Print Confusion Matrix
+        print("Confusion matrix \n \n", confusion_matrix)
+    else:
+        print ('Both lists must be contained the same number of elements')
+# Notifying the user about the error
+except Exception as e:
+    print('Please provide items with same type in the lists!')
