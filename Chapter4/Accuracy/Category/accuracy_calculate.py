e5c7921f6a927a4ccee97e3e8c72dcc90f1a7a5f
naveen.atukorala@pyxeda.ai <naveen.atukorala@pyxeda.ai>
2021-09-03 10:48:59 +0530

    todo instructions added in python file

--- Chapter4/Accuracy/Category/accuracy_calculate.py
+++ Chapter4/Accuracy/Category/accuracy_calculate.py
@@ -1,2 +1,2 @@
-# TODO: Calculate the Accuracy for the 3 category case
+# TODO: Give two lists. list _1 is labels and list_2 is AI predictions, calculate the accuracy (3 category case)
 # TODO: Code should be well commented.
--- Chapter5/MeanSquare/root_square_calculate.py
+++ Chapter5/MeanSquare/root_square_calculate.py
@@ -1,2 +1,2 @@
-# TODO: Calculate the Root Mean Square Error
+# TODO: Give two lists. list _1 is labels and list_2 is AI predictions, calculate the Root Mean Square Error.
 # TODO: Code should be well commented.
