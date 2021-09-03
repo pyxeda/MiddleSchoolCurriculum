d138a23a38474c2944363d79348e394076563f70
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-09-03 13:10:49 +0530

    validation of inputs

--- Chapter4/Accuracy/Binary/accuracy_calculate.py
+++ Chapter4/Accuracy/Binary/accuracy_calculate.py
@@ -1,6 +1,7 @@
 # Python code to calculate the accuracy (binary case), for given two lists. 
 # list _1 is actual labels and list_2 is AI predictions. 
 
+
 # ------------------------------------ Initialise the variables ----------------------------------------
 
 # Initialise the variable to count the no. of actual labels 
@@ -12,10 +13,10 @@ correctPredictions = 0
 # Initialise the variable to check the validity of the inputs
 inputsValid = False
 
-# -------------------------------------------- Inputs --------------------------------------------------
 
-# Change the lists 'label' and 'aiPredictions' as you needed
+# ------------------------------- Giving inputs and validation of inputs --------------------------------
 
+# Change the lists; 'label' and 'aiPredictions' as you needed
 try:
     # Actual labels
     labels = ['cat', 'cat', 'dog', 'cat', 'dog']
@@ -29,48 +30,53 @@ except Exception as e:
     print (e)
 
 else:
-    # Confirm the user has given valid inputs in each list
-    inputsValid = True
+    # Check whether both lists are contain the same number of elements
+    if len(labels) != len (aiPredictions):
+        # If the number of elements in the 2 lists are are not same,
+        print ('Both lists must be contained the same number of elements')
+    
+    # Check whether the lists are empty
+    elif len(labels) == 0:
+        # If the lists are empty,
+        print ('Lists should not be empty')
+    
+    # Confirm the user has given valid inputs for both lists
+    else:
+        # Make the variable name 'inputsValid' True
+        inputsValid = True
 
 
 # -------------------------------- Accuracy calculation starts here -------------------------------------
 
 # This part will only execute if the given inputs are valid
 if inputsValid:
-    try:   
-        # Check whether both lists contain the same number of elements
-        if len(labels) == len (aiPredictions):
+    try:      
+        # Iterate through the each index of actual labels in the list 'labels'
+        for label in range(len(labels)):
             
-            # Iterate through the each index of actual labels in the list 'labels'
-            for label in range(len(labels)):
+            # Increase the count by 1, after iterated through each actual label
+            count += 1
+            
+            # Check whether the predicted label is same as the actual label           
+            if labels[label] == aiPredictions[label]:
                 
-                # Increase the count by 1, after iterated through each actual label
-                count += 1
-                
-                # Check whether the predicted label is same as the actual label           
-                if labels[label] == aiPredictions[label]:
-                    
-                    # If predicted label is same as the actual label, increase the correctPredictions count by 1
-                    correctPredictions += 1
-                
-                # Calculate the accuracy 
-                accuracy = correctPredictions / count
-                
-                # Calculate the caauracy percentage
-                accuracy_percentage = accuracy * 100
+                # If predicted label is same as the actual label, increase the correctPredictions count by 1
+                correctPredictions += 1
+            
+            # Calculate the accuracy 
+            accuracy = correctPredictions / count
+            
+            # Calculate the caauracy percentage
+            accuracy_percentage = accuracy * 100
 
-            # Print the accuracy
-            print ('Accuaracy: ', accuracy)
-            
-            # Print the percentage of the accuracy
-            print ('Accuracy Percentage: ' + str(accuracy_percentage) + ' %')
-            
-        # If the number of elements in the 2 lists are are not same,
-        else:
-            print ('Both lists must be contained the same number of elements')
+        # Print the accuracy
+        print ('Accuaracy: ', accuracy)
+        
+        # Print the percentage of the accuracy
+        print ('Accuracy Percentage: ' + str(accuracy_percentage) + ' %')
 
     except Exception as e:
         # Notifying the user about the error
-        print ('Lists should not be empty')
+        print (e)
         
 
