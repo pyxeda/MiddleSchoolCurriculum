1bf787d7df3f7cf8569af4d81c2e24a5beb9b30d
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-09-07 12:09:51 +0530

    add the license header

--- Chapter4/Accuracy/Category/accuracy_calculate.py
+++ Chapter4/Accuracy/Category/accuracy_calculate.py
@@ -1,3 +1,21 @@
+'''Copyright (c) 2021 AIClub
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without 
+limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
+EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+OR OTHER DEALINGS IN THE SOFTWARE.'''
+
+
 # Python code to calculate the accuracy (3 category case), for given two lists. 
 # list _1 is actual labels and list_2 is AI predictions. 
 
@@ -66,7 +84,7 @@ if inputsValid:
     # Calculate the accuracy 
     accuracy = correctPredictions / count
 
-    # Calculate the caauracy percentage
+    # Calculate the accuracy percentage
     accuracy_percentage = accuracy * 100
 
     # Print the accuracy
