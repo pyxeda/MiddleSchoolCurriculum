35db9f873ba4c02ed5d7830d0deed6b0364df0ac
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-09-07 11:48:25 +0530

    add the license header

--- Chapter3/Image/image_read.py
+++ Chapter3/Image/image_read.py
@@ -1,3 +1,20 @@
+'''Copyright (c) 2021 AIClub
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without 
+limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
+EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+OR OTHER DEALINGS IN THE SOFTWARE.'''
+
 # Python program to read an image and display it
 
 
@@ -8,7 +25,7 @@ import cv2
 import gdown
 
 
-# -------------------------- Get the file location from the google drive. ---------------------------------
+# ----------------------------- Get the file location from the google drive. ---------------------------------
 
 # Please change the url as needed (make sure you have the access to the file)
 url = 'https://drive.google.com/file/d/1CokCuNkyP1zvuTXj_-hxIwevXz4TG9SA/view?usp=sharing'
@@ -32,13 +49,13 @@ try:
   # Read the image
   image = cv2.imread(file_location) 
 
-  # Output image with window name as 'Image' 
+  # Output the image with window name as 'Image' 
   cv2.imshow('Image', image) 
 
-  # Maintain output window utill user presses a key 
+  # Maintain the output window utill user presses a key 
   cv2.waitKey(0)		 
 
-  # Destroying present windows on screen 
+  # Destroying the present windows on screen 
   cv2.destroyAllWindows() 
 
 except Exception as e:
