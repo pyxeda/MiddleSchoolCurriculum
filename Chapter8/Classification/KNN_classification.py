22fa5503b2d5e0a656c9eddb1717bc70c984a8c0
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-09-13 09:46:20 +0530

    create the folder structure for chapter 8

---
+++ Chapter8/Classification/KNN_classification.py
@@ -0,0 +1,3 @@
+# TODO: Create KNN model (using scikit-learn) on the child vs adult dataset (classification). 
+#       Calculate the accuracy and confusion matrix
+# TODO: Code should be well commented.
---
+++ Chapter8/Regression/KNN_regression.py
@@ -0,0 +1,2 @@
+# TODO: Create KNN model (using scikit-learn) on the averages dataset (regression). Calculate RMSE and MAE
+# TODO: Code should be well commented.
