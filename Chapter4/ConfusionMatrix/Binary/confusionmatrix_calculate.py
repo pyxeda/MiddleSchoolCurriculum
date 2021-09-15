ee45ad61ec8114b010e0264a56e5241dce906353
naveen.atukorala@pyxeda.ai <naveen.atukorala@pyxeda.ai>
2021-09-15 16:42:19 +0530

    try-except removed, input lists changed, used confusion matirx from sklearn

--- Chapter4/ConfusionMatrix/Binary/confusionmatrix_calculate.py
+++ Chapter4/ConfusionMatrix/Binary/confusionmatrix_calculate.py
@@ -18,36 +18,22 @@ OR OTHER DEALINGS IN THE SOFTWARE.'''
 # Python program to calucalte the confusion matrix for binary case
 # Binary classification
 
-# Import pandas to calculate confusion matrix
-import pandas as pd
+# Import confusion matrix from sklearn library
+from sklearn.metrics import confusion_matrix
 
 #--------------------------------------------- Lists --------------------------------------------------------------------
 
 # Actuals
-actuals = [1, 1, 1, 1, 1, 0, 0, 1, 1, 1]
+actuals = ['cat', 'cat', 'dog', 'cat', 'dog']
 
 # Predictions
-predictions = [1, 0, 1, 0, 1, 0, 1, 1, 1, 1]
+predictions = ['cat', 'dog', 'dog', 'cat', 'cat'] 
 
-#--------------------------------------------- Calculate the Confusion Matrix --------------------------------------------
+#--------------------------------------------- Construct the Confusion Matrix --------------------------------------------
 
-try:
-    # Check whether both lists contain the same number of elements
-    if len(actuals) == len (predictions):
-        
-        # Convert actual list to a series
-        actuals = pd.Series(actuals, name='Actual')
-        
-        # Convert prediction list to a series
-        predictions = pd.Series(predictions, name='Predictions')
-        
-        # Create Confusion Matrix
-        confusion_matrix = pd.crosstab(predictions, actuals)
+# Construct Confusion Matrix
+constructed_confusion_matrix = confusion_matrix(predictions, actuals)
 
-        # Print Confusion Matrix
-        print("Confusion matrix \n \n", confusion_matrix)
-    else:
-        print ('Both lists must contain the same number of elements')
-# Notifying the user about the error
-except Exception as e:
-    print('All items of the lists, should be of same type')
+# Print Confusion Matrix
+print("Confusion matrix \n \n", constructed_confusion_matrix)
+    
