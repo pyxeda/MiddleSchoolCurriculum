ba568ca4531c6956303361419e8360cc47682b53
ChathukiKet <chathuki.navanjana@pyxeda.ai>
2021-09-28 14:10:07 +0530

    folder structure added for chapter 9, 10

---
+++ Chapter10/LogisticRegression/logistic_regression.py
@@ -0,0 +1,3 @@
+# TODO: Create a logistic regression model (using scikit learn) on the child vs adult dataset. 
+#       Calculate the accuracy and confusion matrix
+# TODO: Code should be well commented.
---
+++ Chapter9/RandomForest/Regression/README.md
@@ -0,0 +1,1 @@
+This readme should describe 'what is in the python code'.
